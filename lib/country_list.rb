e2005fd3a6f2a8576f2c8438e44e5c9184a89202
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-21 11:28:26 -0600

    kept commented alt methods for country list bug fix

--- lib/country_list.rb
+++ lib/country_list.rb
@@ -20,9 +20,28 @@ class CountryList
     all.map { |country| country[:continent] }.uniq.sort
   end
 
+  # original .each method
+
+  # def countries_for_continent(continent)
+  #   countries_in_continent = []
+  #   all.each do |hash|
+  #     if hash[:continent] == continent
+  #       countries_in_continent << hash
+  #     end
+  #   end
+  #   countries_in_continent
+  # end
+
+  # Method using .reject
+
+  # def countries_for_continent(continent)
+  #   all.reject {|hash| hash[:continent] != continent }
+  # end
+
+  # reversed using .select
 
   def countries_for_continent(continent)
-    all.reject {|hash| hash[:continent] != continent }
+    all.select {|hash| hash[:continent] == continent }
   end
 
 end
